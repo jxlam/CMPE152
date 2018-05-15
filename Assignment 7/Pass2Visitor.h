1ceaaa83a46894bc779b64f06e6f6dad0eef182d
jxlam <36719011+jxlam@users.noreply.github.com>
2018-05-14 18:57:49 -0700

    Update Pass2Visitor.h

--- Assignment 7/Pass2Visitor.h
+++ Assignment 7/Pass2Visitor.h
@@ -8,14 +8,14 @@
 #include "wci/intermediate/SymTabEntry.h"
 #include "wci/intermediate/TypeSpec.h"
 
-#include "Pcl2BaseVisitor.h"
+#include "GheyBaseVisitor.h"
 #include "antlr4-runtime.h"
 #include "GheyVisitor.h"
 
 using namespace wci;
 using namespace wci::intermediate;
 
-class Pass2Visitor : public Pcl2BaseVisitor
+class Pass2Visitor : public GheyBaseVisitor
 {
 private:
 	string program_name;
@@ -25,17 +25,15 @@ public:
 	Pass2Visitor(ostream& j_file);
     virtual ~Pass2Visitor();
 
-    antlrcpp::Any visitProgram(Pcl2Parser::ProgramContext *ctx) override;
-    antlrcpp::Any visitHeader(Pcl2Parser::HeaderContext *ctx) override;
-    antlrcpp::Any visitMainBlock(Pcl2Parser::MainBlockContext *ctx) override;
-    antlrcpp::Any visitStmt(Pcl2Parser::StmtContext *ctx) override;
-    antlrcpp::Any visitAssignmentStmt(Pcl2Parser::AssignmentStmtContext *ctx) override;
-    antlrcpp::Any visitAddSubExpr(Pcl2Parser::AddSubExprContext *ctx) override;
-    antlrcpp::Any visitMulDivExpr(Pcl2Parser::MulDivExprContext *ctx) override;
-    antlrcpp::Any visitVariableExpr(Pcl2Parser::VariableExprContext *ctx) override;
-    antlrcpp::Any visitSignedNumber(Pcl2Parser::SignedNumberContext *ctx) override;
-    antlrcpp::Any visitIntegerConst(Pcl2Parser::IntegerConstContext *ctx) override;
-    antlrcpp::Any visitFloatConst(Pcl2Parser::FloatConstContext *ctx) override;
+    antlrcpp::Any visitProg(GheyParser::ProgContext *ctx) override;
+    antlrcpp::Any visitStmt(GheyParser::StmtContext *ctx) override;
+    antlrcpp::Any visitCompound_stmt(GheyParser::Compound_stmtContext *ctx) override;
+    antlrcpp::Any visitAssignment_stmt(GheyParser::Assignment_stmtContext *ctx) override;
+    antlrcpp::Any visitIf_stmt(GheyParser::If_stmtContext *ctx) override;
+    antlrcpp::Any visitMul_div_op(GheyParser::Mul_div_opContext *ctx) override;
+    antlrcpp::Any visitAdd_sub_op(GheyParser::Add_sub_opContext *ctx) override;
+    antlrcpp::Any visitRel_op(GheyParser::Rel_opContext *ctx) override;
+    antlrcpp::Any visitNumber(GheyParser::NumberContext *ctx) override;
 };
 
 #endif /* PASS2VISITOR_H_ */
